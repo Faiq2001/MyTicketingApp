051f8fa0e0dae73041295eccf0b38b03a10e9926
Faiq2001 <faiqahmadmalik@gmail.com>
2024-11-03 19:29:31 -0800

    Trying to start it up in new system

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '@rallycoding/common';
+import { currentUser } from '@fa1/common';
 
 const router = express.Router();
 
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,7 +5,7 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
-} from '@rallycoding/common';
+} from '@fa1/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
 import { natsWrapper } from '../nats-wrapper';
