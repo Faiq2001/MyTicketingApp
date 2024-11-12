691856d9d14dc9562ab321795d5a858e23f3d0c7
Faiq2001 <faiqahmadmalik@gmail.com>
2024-11-12 12:30:13 -0800

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,6 +1,7 @@
 export enum Subjects {
   TicketCreated = 'ticket:created',
   TicketUpdated = 'ticket:updated',
+
   OrderCreated = 'order:created',
-  OrderCancelled = 'order:cancelled'
+  OrderCancelled = 'order:cancelled',
 }
